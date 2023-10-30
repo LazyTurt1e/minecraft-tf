5773520e5e4dbf8b934e0edc65186c52c717d6d5
LazyTurtle <mark.simoniants@gmail.com>
2023-10-30 13:35:35 +0100

    text

--- cube/main.tf
+++ cube/main.tf
@@ -22,7 +22,7 @@ terraform {
   required_providers {
     minecraft = {
       source  = "HashiCraft/minecraft"
-      version = "0.1.0"
+      version = "0.1.1"
     }
   }
 }
--- main.tf
+++ main.tf
@@ -2,13 +2,49 @@ terraform {
   required_providers {
     minecraft = {
       source  = "HashiCraft/minecraft"
-      version = "0.1.0"
+      version = "0.1.1"
+    }
+docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
     }
   }
 }
 
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "sirplexus" {
+  name = "sirplexus/minecraft-server-standalone:latest"
+}
+
+# Create a container
+resource "docker_container" "sirplexus" {
+  image = docker_image.sirplexus.image_id
+  name  = "sirplexus"
+}
+
 // Configure the provider with the RCON details of the Minecraft server
 provider "minecraft" {
-  address  = "localhost:27015"
-  password = "password"
+  address  = "localhost:25565"
+  password = "5879"
+}
+
+module "cube" {
+  source = "./cube"
+
+  material = "cobblestone"
+
+  position = {
+    x = 29,
+    y = 70,
+    z = 21
+  }
+
+  dimensions = {
+    width  = 2,
+    length = 2,
+    height = 2
+  }
 }

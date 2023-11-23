bb958a73e33d78cbd3892ce077392f11f25942a9
LazyTurtle <mark.simoniants@gmail.com>
2023-11-23 13:32:17 +0100

    text

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#EXAMPELS
+
 # resource "minecraft_block" "stone" {
 #   material = "minecraft:stone"
 
@@ -36,20 +38,6 @@
 #   }
 # }
 
-# module "pig" { 
-#  source = "./entity"
-
-#  entity = "pig"
-  
-#  amount=3
-
-#  location = {
-#    x = 224,
-#    y = 81,
-#    z = 2024
-#   }
-
-# }
 
 
 
@@ -58,109 +46,4 @@
 
 
 
-module "floor_for_test" {
-  source = "./cube"
 
-  material = "smooth_stone"
-
-  position = {
-    x = 220,
-    y = 69,
-    z = 2020
-  }
-
-  dimensions = {
-    width  = 10,
-    length = 10,
-    height = 1
-  }
-}
-
-module "hollow-cube" {
-  source = "git::https://github.com/DevOpsPlayground/terraform_minecraft_playground.git//square"
-  material = "minecraft:stone"
-
-  position = {
-    x = 222,
-    y = 70,
-    z = 2022
-  }
-
-  dimensions = {
-    width  = 6,
-    length = 6,
-    height = 4
-  }
-}
-
-module "top_for_test" {
-  source = "./cube"
-
-  material = "smooth_stone"
-
-  position = {
-    x = 220,
-    y = 74,
-    z = 2020
-  }
-
-  dimensions = {
-    width  = 10,
-    length = 10,
-    height = 1
-  }
-}
-
-module "middel" {
-  source = "./cube"
-
-  material = "water"
-
-  position = {
-    x = 224,
-    y = 71,
-    z = 2024
-  }
-
-  dimensions = {
-    width  = 2,
-    length = 2,
-    height = 1
-  }
-}
-
-module "water_holder" {
-  source = "git::https://github.com/DevOpsPlayground/terraform_minecraft_playground.git//square"
-
-  material = "chiseled_stone_bricks"
-
-  position = {
-    x = 223,
-    y = 70,
-    z = 2023
-  }
-
-  dimensions = {
-    width  = 4,
-    length = 4,
-    height = 2
-  }
-}
-
-module "torch_inside" {
-  source = "git::https://github.com/DevOpsPlayground/terraform_minecraft_playground.git//square"
-
-  material = "torch"
-
-  position = {
-    x = 223,
-    y = 72,
-    z = 2023
-  }
-
-  dimensions = {
-    width  = 4,
-    length = 4,
-    height = 1
-  }
-}
